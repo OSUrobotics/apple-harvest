d00183e2b2a1e54f4129e20c3b898eaeecdc4139
cravetzm <66500001+cravetzm@users.noreply.github.com>
2024-10-03 16:46:53 -0700

    added pull twist status topic

--- harvest_control/src/pull_twist_controller.py
+++ harvest_control/src/pull_twist_controller.py
@@ -2,6 +2,7 @@ import numpy as np
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Vector3, TwistStamped, TransformStamped
+from std_msgs.msg import Bool
 from std_srvs.srv import Empty
 from scipy.spatial.transform import Rotation
 from rcl_interfaces.msg import Parameter, ParameterValue, ParameterType
@@ -18,6 +19,7 @@ class PTController(Node):
 
         self.pose_subscription = self.create_subscription(TransformStamped,'/tool_pose', self.configure_self, 10)
         self.publisher = self.create_publisher(TwistStamped, '/servo_node/delta_twist_cmds', 10)
+        self.status_publisher = self.create_publisher(Bool, '/pull_twist/status', 10)
 
         self.timer = self.create_timer(0.01, self.timer_callback)
         
