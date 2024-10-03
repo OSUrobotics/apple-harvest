0d162b98fe164f5eec646bfee1bbbfe30b38cbca
cravetzm <66500001+cravetzm@users.noreply.github.com>
2024-10-03 15:52:16 -0700

    removed WUR-only code

--- harvest_control/src/pressure_averager.py
+++ harvest_control/src/pressure_averager.py
@@ -10,12 +10,10 @@ class PressureAverager(Node):
 
         super().__init__('pressure_averager')
 
-        #self.s1_subscriber = self.create_subscription(UInt16, '/gripper/pressure/sc1', self.log_sensor1, 10)
-        #self.s2_subscriber = self.create_subscription(UInt16, '/gripper/pressure/sc2', self.log_sensor2, 10)
-        #self.s3_subscriber = self.create_subscription(UInt16, '/gripper/pressure/sc3', self.log_sensor3, 10)
+        self.s1_subscriber = self.create_subscription(UInt16, '/gripper/pressure/sc1', self.log_sensor1, 10)
+        self.s2_subscriber = self.create_subscription(UInt16, '/gripper/pressure/sc2', self.log_sensor2, 10)
+        self.s3_subscriber = self.create_subscription(UInt16, '/gripper/pressure/sc3', self.log_sensor3, 10)
         
-        self.subscriber = self.create_subscription(UInt16, '/gripper/pressure', self.log_pressure, 10)
-
         self.publisher = self.create_publisher(UInt16, '/pressure', 10)
         self.timer = self.create_timer(0.01, self.timer_callback)
 
@@ -29,9 +27,6 @@ class PressureAverager(Node):
 
     def log_sensor3(self, msg):
         self.pressures[2] = msg.data
-
-    def log_pressure(self,msg):
-        self.pressure = msg.data
         
     def timer_callback(self):
         out_pressure = UInt16()
