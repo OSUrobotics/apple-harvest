ffa304c88ead18ef9aa8edec00f5e1bbe283b44a
Alejo Velasquez <velasale@oregonstate.edu>
2024-10-14 10:07:16 -0700

    suction gripper

--- harvest_control/scripts/alejo_file_test.py
+++ harvest_control/scripts/alejo_file_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import numpy as np
-import pandas as pd
+#import pandas as pd
 import scipy
 from scipy.ndimage import median_filter
 import os
@@ -19,24 +19,25 @@ from ament_index_python.packages import get_package_share_directory
 from std_srvs.srv import Trigger, Empty
 from geometry_msgs.msg import Point
 from visualization_msgs.msg import Marker, MarkerArray
-from harvest_interfaces.srv import TextInRviz, BackAndForth
+from harvest_interfaces.srv import TextInRviz
+from gripper_msgs.srv import GripperVacuum, GripperFingers
 
 class AlejoTrial(Node):
     def __init__(self):
         super().__init__('alejo_trial')
 
-        # Create services (type, name, function)
+        # Services (type, name, function)
         self.text_in_rviz_srv = self.create_service(TextInRviz, 'place_text_in_rviz', self.toy_problem_callback)
-        self.graps_apple_srv = self.create_service(GraspApple, 'grasp_apple', self.grasp_apple_callback)
+        self.graps_apple_srv = self.create_service(Empty, 'grasp_apple', self.grasp_apple_callback)
 
-        # Create service clients.
+        # Service clients
         self.vacuum_service_client = self.create_client(GripperVacuum, 'set_vacuum_status')
-        self.get_logger().info("Waiting for gripper vaccuum server")
-        self.vacuum_service_client.wait_for_service()
+        while not self.vacuum_service_client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info("Waiting for gripper vaccuum server")
 
         self.fingers_service_client = self.create_client(GripperFingers, 'set_fingers_status')
-        self.get_logger().info("Waiting for gripper finger server")
-        self.fingers_service_client.wait_for_service()
+        while not self.fingers_service_client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info("Waiting for gripper finger server")
 
         # Create publisher
         self.marker_text_publisher = self.create_publisher(Marker, 'caption', 10)
@@ -74,46 +75,76 @@ class AlejoTrial(Node):
         response.success = True
 
         return response
-    
-    def grasp_apple_callback_pending(self, request, response):
 
-        # Air-Pressure servoing Parameters
-        MAX_ATTEMPTS = 5
-        PRESSURE_READINGS = 10
-        ENGAGEMENT_THRESHOLD = 600    # Units hPa
 
-        # Approach fruit in straight line
-        # TODO: linear approach and turn vacuum on
-        # TODO: Subscribe to the toF distance
-        self.get_logger().info(f'Initial linear approach to apple')               
+    def grasp_apple_callback(self, request, response):
 
-        # Control loop
-        cnt = 0
-        while cnt < MAX_ATTEMPTS:
+        # Make the service call: 'Vacuum on'
+        self.request = GripperVacuum.Request()
+        self.request.set_vacuum = True
+        self.future = self.vacuum_service_client.call_async(self.request)
+        #rclpy.spin_until_future_complete(self, self.future)
 
-            # Air Pressure Readings
-            ps1_list = []
-            ps2_list = []
-            ps3_list = []
-            for i in range(PRESSURE_READINGS):
-                # TODO: Have the gripper publishing the air-pressure readings
-                # TODO: Have this node subscribed to the air-pressure readings
-                ps1_list.append(TODO)
-                ps2_list.append(TODO)
-                ps3_list.append(TODO)
-            ps1_mean = np.mean(ps1_list)
-            ps2_mean = np.mean(ps2_list)
-            ps3_mean = np.mean(ps3_list)
-            self.get_logger().info(f'Mean Air Pressure Readings: {int(ps1_mean)}, {int(ps2_mean)}, {int(ps3_mean)}')
+        # Make service call: 'Engage Fingers'
+        self.request = GripperFingers.Request()
+        self.request.set_fingers = True
+        self.future = self.fingers_service_client.call_async(self.request)
+        #rclpy.spin_until_future_complete(self, self.future)
 
-            if ps1_mean < ENGAGEMENT_THRESHOLD and ps2_mean < ENGAGEMENT_THRESHOLD and ps3_mean < ENGAGEMENT_THRESHOLD:
-                self.get_logger().info(f'All suction cups are engaged')
-                break
+        # Make service call to move the arm back
 
-            # Adjust Pose
-            self.get_logger().info(f'Adjusting angular pose of gripper')
-            theta, omega = self.rotation_axis_angle(ps1_mean, ps2_mean, ps3_mean) 
-            cr_x, cr_y = 
+        # Make service call to Disengage Fingers
+        self.request = GripperFingers.Request()
+        self.request.set_fingers = False
+        self.future = self.fingers_service_client.call_async(self.request)
+
+
+        # Make service call: 'Vacuum off'
+        self.request = GripperVacuum.Request()
+        self.request.set_vacuum = False
+        self.future = self.vacuum_service_client.call_async(self.request)
+
+        return response
+
+
+    # def grasp_apple_callback_pending(self, request, response):
+        # # Air-Pressure servoing Parameters
+        # MAX_ATTEMPTS = 5
+        # PRESSURE_READINGS = 10
+        # ENGAGEMENT_THRESHOLD = 600    # Units hPa
+        #
+        # # Approach fruit in straight line
+        # # TODO: linear approach and turn vacuum on
+        # # TODO: Subscribe to the toF distance
+        # self.get_logger().info(f'Initial linear approach to apple')
+        #
+        # # Control loop
+        # cnt = 0
+        # while cnt < MAX_ATTEMPTS:
+        #
+        #     # Air Pressure Readings
+        #     ps1_list = []
+        #     ps2_list = []
+        #     ps3_list = []
+        #     for i in range(PRESSURE_READINGS):
+        #         # TODO: Have the gripper publishing the air-pressure readings
+        #         # TODO: Have this node subscribed to the air-pressure readings
+        #         ps1_list.append(TODO)
+        #         ps2_list.append(TODO)
+        #         ps3_list.append(TODO)
+        #     ps1_mean = np.mean(ps1_list)
+        #     ps2_mean = np.mean(ps2_list)
+        #     ps3_mean = np.mean(ps3_list)
+        #     self.get_logger().info(f'Mean Air Pressure Readings: {int(ps1_mean)}, {int(ps2_mean)}, {int(ps3_mean)}')
+        #
+        #     if ps1_mean < ENGAGEMENT_THRESHOLD and ps2_mean < ENGAGEMENT_THRESHOLD and ps3_mean < ENGAGEMENT_THRESHOLD:
+        #         self.get_logger().info(f'All suction cups are engaged')
+        #         break
+        #
+        #     # Adjust Pose
+        #     self.get_logger().info(f'Adjusting angular pose of gripper')
+        #     theta, omega = self.rotation_axis_angle(ps1_mean, ps2_mean, ps3_mean)
+        #     cr_x, cr_y =
 
     def rotation_axis_angle(gp1, gp2, gp3):
         """
