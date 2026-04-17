2e1fe489737953055b9a090ae060343a7846d4f2
Marcus <rosettem@oregonstate.edu>
2026-04-17 13:42:46 -0700

    revises apple prediction nodes to work with presaved images for realsense and azure

--- harvest/launch/launch_vision_templating.launch.py
+++ harvest/launch/launch_vision_templating.launch.py
@@ -12,18 +12,20 @@ def generate_launch_description():
     ### apple_prediction node parameters & voxelize_scan (vision_experiment)
     # Segmentation model is trained on open source datasets and can give slightly more accurate 3D reconstruction results. 
     # detection models are trained on Prosser data and may be more robust to field conditions.
-    declared_arguments.append(DeclareLaunchArgument("prediction_model", default_value="best_segmentation.pt", 
+    declared_arguments.append(DeclareLaunchArgument("prediction_model", default_value="v9e.pt", 
                                   description="Yolo model used, can specify any model in the harvest_vision/yolo_models directory."))
-    declared_arguments.append(DeclareLaunchArgument("prediction_yolo_conf", default_value="0.85", 
+    declared_arguments.append(DeclareLaunchArgument("prediction_yolo_conf", default_value="0.75", 
                                   description="Confidence threshold for yolo model in apple_prediction node."))
     declared_arguments.append(DeclareLaunchArgument("prediction_radius_min", default_value="0.03", 
                                   description="Minimum radius bound (meters) for ransac sphere fit in apple_prediction node."))
     declared_arguments.append(DeclareLaunchArgument("prediction_radius_max", default_value="0.06", 
                                   description="Maximum radius bound (meters) for ransac sphere fit in apple_prediction node."))
-    declared_arguments.append(DeclareLaunchArgument("prediction_distance_max", default_value="1.0", 
+    declared_arguments.append(DeclareLaunchArgument("prediction_distance_max", default_value="4.0", 
                                   description="Distance threshold in meters for detecting apples. Filters out backgound apples."))
     declared_arguments.append(DeclareLaunchArgument("vision_experiment", default_value="a", 
                                   description="Microsoft Azure Kinect camera frame (color image, depth image, and point cloud) used in apple detection and voxelize scan."))
+    declared_arguments.append(DeclareLaunchArgument("camera_type", default_value="realsense",
+                                    description="Type of camera used for apple detection. Options are 'azure' or 'realsense'. This affects the camera intrinsics used in the apple_prediction node."))
 
     ### getting paths to yolo_networks
     declared_arguments.append(DeclareLaunchArgument('prediction_model_path', default_value=[PathJoinSubstitution([FindPackageShare("harvest_vision"), "yolo_networks", LaunchConfiguration("prediction_model")])]))
@@ -40,6 +42,7 @@ def generate_launch_description():
                      "prediction_radius_max": LaunchConfiguration("prediction_radius_max"),
                      "prediction_distance_max": LaunchConfiguration("prediction_distance_max"),
                      "vision_experiment": LaunchConfiguration("vision_experiment"),
+                     "camera_type": LaunchConfiguration("camera_type"),
                       }
                 ])
     
@@ -49,6 +52,7 @@ def generate_launch_description():
                 name="voxelize_scan",
                 parameters=[
                     {"vision_experiment": LaunchConfiguration("vision_experiment"),
+                     "camera_type": LaunchConfiguration("camera_type"),
                       }
                 ])
     
